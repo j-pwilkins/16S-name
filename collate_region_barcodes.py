e3809cff86b3cb35a7593c15b0a67500b47bd11a
j-pwilkins <112159163+j-pwilkins@users.noreply.github.com>
2023-01-30 11:53:51 +0000

    Add files via upload
    30.1 11.51
    Updated because tree# is now already in the sheets being compiled.
    Program now allows the full region to be mapped too.

--- collate_region_barcodes.py
+++ collate_region_barcodes.py
@@ -1,78 +1,94 @@
 import pandas as pd
 import numpy as np
+import os
+import shutil
+import sys
 from datetime import datetime
 pd.options.display.max_columns = None
 
 def main():
-    Full_df, V1V3_df, V3V4_df, V4_df, column_headers = read_inputs()
-    tree_df = produce_tree_df(Full_df)
-    V1V3_df = update_regional_df(V1V3_df, tree_df, column_headers)
-    V3V4_df = update_regional_df(V3V4_df, tree_df, column_headers)
-    V4_df = update_regional_df(V4_df, tree_df, column_headers)
-    Full_df = format_full_df(Full_df, column_headers)
-    combined_df = pd.concat([Full_df, V1V3_df, V3V4_df, V4_df], axis=0, ignore_index=True)
-    check_combination(Full_df, V1V3_df, V3V4_df, V4_df, combined_df)
+    db_df, V1V3_df, V3V4_df, V4_df, full_df, column_headers = read_inputs()
+    # tree_df = produce_tree_df(Full_df)
+    V1V3_df = update_regional_df(V1V3_df, column_headers)
+    V3V4_df = update_regional_df(V3V4_df, column_headers)
+    V4_df = update_regional_df(V4_df, column_headers)
+    full_df = update_regional_df(full_df, column_headers)
+    db_df = format_db_df(db_df, column_headers)
+    combined_df = pd.concat([db_df, V1V3_df, V3V4_df, V4_df, full_df], axis=0, ignore_index=True)
+    check_combination(db_df, V1V3_df, V3V4_df, V4_df, full_df, combined_df)
     combined_df = format_combined_df(combined_df)
-    write_csvs(V1V3_df, V3V4_df, V4_df, combined_df)
+    write_csvs(V1V3_df, V3V4_df, V4_df, full_df, db_df, combined_df)
 
 def produce_tree_df(Full_df):
     tree_df = Full_df[['Accession Number', 'Tree#']]
     return tree_df
 
-def update_regional_df(V1V3_df, tree_df, column_headers):
-    merged_df = pd.merge(V1V3_df, tree_df, on='Accession Number')
-    merged_df['Seq Length'] = merged_df['16S rRNA sequence'].str.len()
-    merged_df = merged_df.rename(
-        columns={'Similarity(%)': 'Similarity (%)', 'DB Name': 'Vs Name', 'DB Accession Number': 'Vs ID'})
-    merged_df = merged_df[column_headers]
-    return merged_df
+def update_regional_df(updated_df, column_headers):
+    # updated_df = pd.merge(V1V3_df, tree_df, on='Accession Number')
+    updated_df['Seq Length'] = updated_df['16S rRNA sequence'].str.len()
+    updated_df = updated_df.rename(
+        columns={'Q Accession Number': 'Accession Number', 'Q Name': 'Name', 'Similarity(%)': 'Similarity (%)', 'DB Name': 'Vs Name', 'DB Accession Number': 'Vs ID'})
+    write_csv(updated_df, 'updatetest.csv')
+    updated_df = updated_df[column_headers]
+    return updated_df
 
-def format_full_df(Full_df, column_headers):
-    Full_df = Full_df.rename(
+def format_db_df(db_df, column_headers):
+    db_df = db_df.rename(
         columns={'DB#': 'Query#'})
-    Full_df = Full_df[column_headers]
-    return Full_df
+    db_df = db_df[column_headers]
+    return db_df
 
-def check_combination(Full_df, V1V3_df, V3V4_df, V4_df, combined_df):
-    Full_length = len(Full_df)
+def check_combination(db_df, V1V3_df, V3V4_df, V4_df, full_df, combined_df):
+    db_length = len(db_df)
     V1V3_length = len(V1V3_df)
     V3V4_length = len(V3V4_df)
     V4_length = len(V4_df)
+    full_length = len(full_df)
     Combined_length = len(combined_df)
-    print(f'The Full df is {Full_length} rows long, V1V3 df is {V1V3_length} rows, V3V4 is {V3V4_length}, V4 is {V4_length} and the Combined df is {Combined_length}.')
+    print(f'The Database df is {db_length} rows long, V1V3 df is {V1V3_length} rows, V3V4 is {V3V4_length}, V4 is {V4_length}, Full is {full_length} and the Combined df is {Combined_length}.')
+
 
 def format_combined_df(combined_df):
-    combined_df['ID?'] = combined_df['Region'].apply(lambda x: '16S' if x == 'Full' else 'N')
-    combined_df['ID Order'] = combined_df['ID?'].apply(lambda x: 1 if x == '16S' else 2)
-    combined_df['Selected Order'] = combined_df['Selected'].apply(lambda
-                                                                      x: 1 if x == '16S' else 2 if x == 'Y' else 3 if x == 'Y.' else 4 if x == 'N' else 5 if x == 'Cs' else 6 if x == 'Cm' else 7 if x == 'Cp' else 8 if x == 'Y-na' else 'NA')
-    combined_df['Region Order'] = combined_df['Region'].apply(
-        lambda x: 1 if x == 'Full' else 2 if x == 'V1V3' else 3 if x == 'V3V4' else 4 if x == 'V4' else 'NA')
+    # combined_df['ID?'] = combined_df['Region'].apply(lambda x: '16S' if x == 'Full' else 'N')
+    # combined_df['ID Order'] = combined_df['ID?'].apply(lambda x: 1 if x == '16S' else 2)
+    combined_df['Selected Order'] = combined_df['Selected'].apply(lambda x: 1 if x == 'DB' else 2 if x == 'Y' else 3 if x == 'Y.' else 4 if x == 'N' else 5 if x == 'Cs' else 6 if x == 'Cm' else 7 if x == 'Cp' else 8 if x == 'Y-na' else 'NA')
+    combined_df['Region Order'] = np.where(combined_df['Selected'] == 'DB', 1,
+                                           np.where(combined_df['Region'] == 'Full', 2,
+                                                    np.where(combined_df['Region'] == 'V1V3', 3,
+                                                             np.where(combined_df['Region'] == 'V3V4', 4,
+                                                                      np.where(combined_df['Region'] == 'V4', 5, 'NA')))))
     combined_df['Tree Order'] = combined_df['Tree#']
-    combined_df = combined_df.sort_values(by=['Query#', 'Tree Order', 'Region Order', 'Selected Order', 'ID Order'], ascending=[True, True, True, True, True])
+    # combined_df = combined_df.sort_values(by=['Query#', 'Tree Order', 'Region Order', 'Selected Order', 'ID Order'], ascending=[True, True, True, True, True])
+    combined_df = combined_df.sort_values(by=['Query#', 'Tree Order', 'Region Order', 'Selected Order'],
+                                          ascending=[True, True, True, True])
     combined_df['JP Order'] = np.arange(1, len(combined_df) + 1)
     return combined_df
 
-def write_csvs(V1V3_df, V3V4_df, V4_df, combined_df):
+
+
+def write_csvs(V1V3_df, V3V4_df, V4_df, full_df, db_df, combined_df):
     write_csv(V1V3_df, 'V1V3.csv')
     write_csv(V3V4_df, 'V3V4.csv')
     write_csv(V4_df, 'V4.csv')
+    write_csv(full_df, 'Full.csv')
+    write_csv(db_df, 'DB.csv')
     write_csv(combined_df, 'combined.csv')
 
 ## functions for produce_shared_matches_df
 # read the inputs produced from mapping - these will be passed to the function when integrating into the the new mapping.py
 def read_inputs():
-    Full_df = pd.read_csv('full_16S_database.csv', na_filter=False)
+    db_df = pd.read_csv('full_16S_database.csv', na_filter=False)
     # V1V3_df = read_csv('mapped_barcodes_for_V1V3.csv')
-    V1V3_df = pd.read_csv('mapped_barcodes_for_V1V3.csv', na_filter=False)
-    V3V4_df = pd.read_csv('mapped_barcodes_for_V3V4.csv', na_filter=False)
-    V4_df = pd.read_csv('mapped_barcodes_for_V4.csv', na_filter=False)
+    V1V3_df = pd.read_csv('Complete_list_of_sequences_V1V3_vs_db_497.csv', na_filter=False)
+    V3V4_df = pd.read_csv('Complete_list_of_sequences_V3V4_vs_db_497.csv', na_filter=False)
+    V4_df = pd.read_csv('Complete_list_of_sequences_V4_vs_db_497.csv', na_filter=False)
+    full_df = pd.read_csv('Complete_list_of_sequences_Full_vs_db_497.csv', na_filter=False)
     column_headers = ['Query#', 'Hun#', 'Fas#', 'Tree#', 'Accession Number', 'Name', 'Region', 'Shared Matches', 'Selected', 'Similarity (%)', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Vs DB#', 'Vs Name', 'Vs ID', 'Seq Length', '16S rRNA sequence']
     output_columns = ['Query#', 'Hun#', 'Fas#', 'Accession Number', 'Name', 'Shared Matches', 'Selected', 'Similarity(%)',
                     'Vs DB#',
                     'DB Name', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
                     'DB Accession Number', '16S rRNA sequence']
-    return Full_df, V1V3_df, V3V4_df, V4_df, column_headers
+    return db_df, V1V3_df, V3V4_df, V4_df, full_df, column_headers
 
 ## helper functions
 def read_csv(filename):
@@ -92,39 +108,6 @@ def read_csv(filename):
 def write_csv(df, filename):
     df.to_csv(filename, sep=',', index=False)
 
-def check_unique_values(df1, df2, column_header):
-    unique_values_df1 = df1[column_header].unique().tolist()
-    unique_values_df1.sort()
-    unique_values_df2 = df2[column_header].unique().tolist()
-    unique_values_df2.sort()
-    extra_unique_values_df1 = list(set(unique_values_df1) - set(unique_values_df2))
-    extra_unique_values_df2 = list(set(unique_values_df2) - set(unique_values_df1))
-    unique_values_in_both_lists = list(set(unique_values_df1).intersection(unique_values_df2))
-    number_of_shared_unique_values = len(unique_values_in_both_lists)
-    if unique_values_df1 == unique_values_df2:
-        print(f"{df1.name} and {df2.name} are compatible. They contain {number_of_shared_unique_values} unique values in the '{column_header}' column.")
-    elif len(extra_unique_values_df1) > 0 and len(extra_unique_values_df2) > 0:
-        print(f"{df1.name} and {df2.name} both contain extra unique values in the '{column_header}' column. They share {number_of_shared_unique_values} unique values. {df1.name} has {len(extra_unique_values_df1)} extra unique values. {df2.name} has {len(extra_unique_values_df2)} extra unique values.")
-    elif len(extra_unique_values_df1) > 0 and len(extra_unique_values_df2) == 0:
-        print(f"{df1.name} contains extra unique values in the '{column_header}' column. They share {number_of_shared_unique_values} unique values. {df1.name} has {len(extra_unique_values_df1)} extra unique values.")
-    elif len(extra_unique_values_df1) == 0 and len(extra_unique_values_df2) > 0:
-        print(f"{df2.name} contains extra unique values in the '{column_header}' column. They share {number_of_shared_unique_values} unique values. {df2.name} has {len(extra_unique_values_df2)} extra unique values.")
-
-def check_df_column_unique_entries_only(df, column_header):
-    if df[column_header].duplicated().any():
-        duplicated_rows = df[column_header].duplicated().sum()
-        print(f"{duplicated_rows} queries in the {df.name} '{column_header}' column are duplicated")
-    else:
-        print(f"All entries in the {df.name} '{column_header}' column are unique")
-
-def concatenate_dfs(*dfs):
-    column_headers = [list(df.columns) for df in dfs]
-    if all(headers == column_headers[0] for headers in column_headers):
-        concatenated_df = pd.concat(dfs)
-        return concatenated_df
-    else:
-        df_names = ', '.join([str(df) for df in dfs])
-        print(f"{df_names} could not be concatenated as the column headers do not match. Please rectify")
 
 
 main()
