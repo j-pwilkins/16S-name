62fffabaa7c6b1fba1cfd5d023638e5ef41bc975
j-pwilkins <112159163+j-pwilkins@users.noreply.github.com>
2023-02-14 12:19:48 +0000

    Add files via upload
    14.2 12.18
    Adds the fourth plot. I have done this by scattering invisible data points. It doesn't work without these, I don't know why.

--- produce_scatter_log.py
+++ produce_scatter_log.py
@@ -8,51 +8,67 @@ def main():
 
 def produce_scatter_plot(data_df):
     plt.figure(figsize=(12, 10))
+    # These are the limits selected for the non-log version. Chosen as 0.3 max value and leaves room from top of chart
+    xlim = (0, 0.31)
+    ylim = (0, 0.31)
     plot_region(data_df, 'Full', 'V1V3', 'blue', 1, 'V1V3 Mapped Scores vs Full 16S Mapped Scores',
-                'Full 16S Mapped Scores', 'V1V3 Mapped Scores', (0, 0.31), (0, 0.31), plot_color="b-")
+                'Full 16S Mapped Scores', 'V1V3 Mapped Scores', xlim, ylim, plot_color="b-")
     plot_region(data_df, 'Full', 'V3V4', 'green', 2, 'V3V4 Mapped Scores vs Full 16S Mapped Scores',
-                'Full 16S Mapped Scores', 'V3V4 Mapped Scores', (0, 0.31), (0, 0.31), plot_color="g-")
+                'Full 16S Mapped Scores', 'V3V4 Mapped Scores', xlim, ylim, plot_color="g-")
     plot_region(data_df, 'Full', 'V4', 'red', 3, 'V4 Mapped Scores vs Full 16S Mapped Scores',
-                'Full 16S Mapped Scores', 'V4 Mapped Scores', (0, 0.31), (0, 0.31), plot_color="r--")
-    plot_trendlines(data_df)
+                'Full 16S Mapped Scores', 'V4 Mapped Scores', xlim, ylim, plot_color="r--")
+    plot_trendlines(data_df, xlim, ylim)
     plt.tight_layout()
     plt.show()
 
-def plot_region(df, x, y, color, subplot_index, title, xlabel, ylabel, xlim, ylim, plot_line=True, linewidth=0.8, dashes=[2, 5], plot_color=None):
+def plot_region(df, x, y, color, subplot_index, title, xlabel, ylabel, xlim, ylim, plot_color=None):
     plt.subplot(2, 2, subplot_index)
     plt.scatter(df[x], df[y], color=color, s=2)
     plt.title(title)
     plt.xlabel(xlabel, color='orange')
     plt.ylabel(ylabel, color=color, fontweight='bold')
+    # plots the log scales onto the axes
     plt.xscale("log")
     plt.yscale("log")
+    # set limits of chart - I don't know how this works, but without these lines plot is confined to portion of plot area
     plt.xlim(xlim)
     plt.ylim(ylim)
+    # transforms the data. 0.00000001 is added to each value because there are zeroes which otherwise break the code
     x_log = np.log10(df[x] + 0.00000001)
     y_log = np.log10(df[y] + 0.00000001)
+    # fit the trendlines
     z = np.polyfit(x_log, y_log, 1)
     p = np.poly1d(z)
     x_fit = np.linspace(x_log.min(), x_log.max(), 50)
     y_fit = 10 ** p(x_fit)
     plt.plot(10 ** x_fit, y_fit, plot_color, linewidth=0.8, dashes=[2, 5])
 
-
-def plot_trendlines(data_df):
+def plot_trendlines(df, xlim, ylim):
     plt.subplot(2, 2, 4)
-    z = np.polyfit(data_df['Full'], data_df['V1V3'], 1)
-    p = np.poly1d(z)
-    plt.plot(data_df['Full'], p(data_df['Full']), "b--", linewidth=0.8, dashes=[2, 5])
-    z = np.polyfit(data_df['Full'], data_df['V3V4'], 1)
-    p = np.poly1d(z)
-    plt.plot(data_df['Full'], p(data_df['Full']), 'g--', linewidth=0.8, dashes=[2, 5])
-    z = np.polyfit(data_df['Full'], data_df['V4'], 1)
-    p = np.poly1d(z)
-    plt.plot(data_df['Full'], p(data_df['Full']), 'r--', linewidth=0.8, dashes=[2, 5])
-    plt.xlim(0, 0.31)
-    plt.ylim(0, 0.31)
+    plt.scatter(df['Full'], df['V1V3'], color='blue', s=0)
+    plt.scatter(df['Full'], df['V3V4'], color='green', s=0)
+    plt.scatter(df['Full'], df['V4'], color='red', s=0)
+    plt.title('Regional Trendlines for Mapped Scores')
     plt.xlabel('Full 16S Mapped Scores', color='orange')
     plt.ylabel('Regional Mapped Scores', color='black', fontweight='bold')
-    plt.title('Regional Trendlines for Mapped Scores')
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plot_trendline(df['Full'], df['V1V3'], "b")
+    plot_trendline(df['Full'], df['V3V4'], "g")
+    plot_trendline(df['Full'], df['V4'], "r")
+
+
+def plot_trendline(x, y, color):
+    x_log = np.log10(x + 0.00000001)
+    y_log = np.log10(y + 0.00000001)
+    z = np.polyfit(x_log, y_log, 1)
+    p = np.poly1d(z)
+    x_fit = np.linspace(x_log.min(), x_log.max(), 50)
+    y_fit = 10 ** p(x_fit)
+    plt.plot(10 ** x_fit, y_fit, color + '--', linewidth=0.8, dashes=[2, 5])
+
 
 ## helper functions
 def read_csv(filename):
